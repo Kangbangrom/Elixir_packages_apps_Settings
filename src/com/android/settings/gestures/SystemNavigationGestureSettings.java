7942ebc330dc14557ecc9eecb40241bd46d57f28
S ∆ U R ∆ V <dwarmachine24@gmail.com>
2022-04-15 08:48:12 +0530

    [TEMP] Fixup: Typo in SystemNavigationGestureSettings while picking

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -69,7 +69,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
     public static final String PREF_KEY_SUGGESTION_COMPLETE =
             "pref_system_navigation_suggestion_complete";
 
-    private static final String FULLSCREEN_GESTURE_OVERLAY_PKG = "com.evolution.overlay.systemui.navbar.gestural";
+    private static final String FULLSCREEN_GESTURE_OVERLAY_PKG = "com.krypton.overlay.systemui.navbar.gestural";
     private static final String KEY_SHOW_A11Y_TUTORIAL_DIALOG = "show_a11y_tutorial_dialog_bool";
 
     private boolean mA11yTutorialDialogShown = false;
